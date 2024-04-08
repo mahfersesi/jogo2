428d74ceef245fca06eee52f5e95307fa7068650
maria fernanda <mariafernandalibano345@gmail.com>
2024-04-08 09:07:25 -0300

    erros

--- jogo2/boiPage.xaml.cs
+++ jogo2/boiPage.xaml.cs
@@ -1,2 +1,31 @@
 namespace jogo2;
 
+public partial class boiPage : ContentPage
+{
+	Boi boi;
+	Boi fome;
+	public boiPage()
+	{
+		InitializeComponent();
+		
+		boi= new Boi();
+
+		ImgVaca.Source= boi.Getarquivo();
+	}
+
+	void Botaovaca(object sender, EventArgs args)
+	{
+		boi.SetFome(boi.GetFome()+0.1);
+	}
+
+	void Vacasede(object sender, EventArgs args)
+	{
+		boi.SetSede(boi.GetSede()+0.1);
+	}
+
+	void Botao(object sender, EventArgs args)
+	{
+		boi.SetFeliz(boi.GetFeliz()+0.1);
+	}
+
+}
